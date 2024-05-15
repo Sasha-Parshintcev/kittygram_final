2abdcf6e1d6dfe86efc2b4a6227e7fe430590b41
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-15 20:30:07 +0700

    debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -98,8 +98,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_URL = '/collected_static/'
+STATIC_ROOT = BASE_DIR / 'collected_static/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / '/media'
