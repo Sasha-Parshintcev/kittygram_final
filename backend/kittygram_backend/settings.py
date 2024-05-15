f91d112b97f788b5c9a58c7e2c6943352d5cf123
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-15 15:17:04 +0700

    debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -100,10 +100,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'backend_static/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = (BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
