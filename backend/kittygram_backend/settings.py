380e8c05589728149768b49206ea88479c1c91d0
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-16 12:46:07 +0700

    red settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,9 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '/.env')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '/.env')
 
-ALLOWED_HOSTS = ['158.160.83.75', '127.0.0.1', 'localhost', 'sashamykittygram.zapto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
